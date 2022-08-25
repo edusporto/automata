ee2d02a0360204946c1cb6eeea42770949d0f850
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-08-25 15:38:55 -0300

    Add implementation for DFA and some of its operations
    Adds implementation to
    - Union of DFAs
    - Intersection of DFAs

---
+++ src/Automata/DFA.hs
@@ -0,0 +1,39 @@
+module Automata.DFA (module Automata.DFA) where
+
+import Data.Foldable (foldl')
+
+-- | Represents a Deterministic Finite Automaton.
+--
+-- The state set Q and alphabet set Σ are defined
+-- by the type variables `s` and `a`.
+data DFA s a = DFA
+  { transition :: s -> a -> s,
+    start :: s,
+    endings :: s -> Bool
+  }
+
+run :: DFA s a -> [a] -> s
+run dfa = foldl' (transition dfa) (start dfa)
+
+accepts :: DFA s a -> [a] -> Bool
+accepts dfa string =
+  let lastState = run dfa string
+   in endings dfa lastState
+
+inter :: DFA s1 a -> DFA s2 a -> DFA (s1, s2) a
+inter (DFA δ1 s1 end1) (DFA δ2 s2 end2) = DFA δ (s1, s2) end
+  where
+    δ (r1, r2) a = (δ1 r1 a, δ2 r2 a)
+    end (r1, r2) = end1 r1 && end2 r2
+
+(∩) :: DFA s1 a -> DFA s2 a -> DFA (s1, s2) a
+(∩) = inter
+
+union :: DFA s1 a -> DFA s2 a -> DFA (s1, s2) a
+union (DFA δ1 s1 end1) (DFA δ2 s2 end2) = DFA δ (s1, s2) end
+  where
+    δ (r1, r2) a = (δ1 r1 a, δ2 r2 a)
+    end (r1, r2) = end1 r1 || end2 r2
+
+(∪) :: DFA s1 a -> DFA s2 a -> DFA (s1, s2) a
+(∪) = inter
