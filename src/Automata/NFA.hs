c75c083a820e8c4147500a959a3ed559cf90b85e
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-08-29 22:09:09 -0300

    Add NFA union

--- src/Automata/NFA.hs
+++ src/Automata/NFA.hs
@@ -21,6 +21,11 @@ instance Automaton NFA where
     let lastStates = run nfa string
      in any (endings nfa) lastStates
 
+data StateUnion s1 s2
+  = Start
+  | S1 s1
+  | S2 s2
+
 next :: (s -> Maybe a -> [s]) -> [s] -> a -> [s]
 next δ states symbol =
   states >>= \state ->
@@ -28,3 +33,25 @@ next δ states symbol =
 
 run :: NFA s a -> [a] -> [s]
 run nfa = foldl' (next (transition nfa)) [start nfa]
+
+-- | Union of NFAs
+--
+-- Note that the resulting state set is a sum type of
+-- a unit type, s1, and s2. This means that the resulting
+-- amount of possible states is 1 + |s1| + |s2|.
+--
+-- This is different than the union operation for DFAs,
+-- which results in product type of size |s1| * |s2|.
+unionN :: NFA s1 a -> NFA s2 a -> NFA (StateUnion s1 s2) a
+unionN (NFA δ1 s1 e1) (NFA δ2 s2 e2) = NFA δ Start end
+  where
+    δ q a = case q of
+      Start -> case a of
+        Nothing -> [S1 s1, S2 s2]
+        _ -> []
+      S1 q' -> map S1 (δ1 q' a)
+      S2 q' -> map S2 (δ2 q' a)
+    end q = case q of
+      Start -> False
+      S1 q' -> e1 q'
+      S2 q' -> e2 q'
