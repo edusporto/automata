18e18957d8d941ff4f10151b4e23949b7ceb1458
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-08-31 22:08:04 -0300

    Add experimental definition to the transition function for NFAs

--- src/Automata/Regular/NFA.hs
+++ src/Automata/Regular/NFA.hs
@@ -15,6 +15,28 @@ data NFA s a = NFA
     endings :: s -> Bool
   }
 
+-- | Experimental definition for an NFA.
+--
+-- The transition function in automata theory for an NFA
+-- is of the type Q ⨯ Σ → P(Q), where P(Q) is the power set
+-- of Q, that is, the set of all subsets of Q. The amount of
+-- elements in the power set P(Q) is equal to 2^|Q|, where |Q|
+-- is the amount of elements in Q.
+--
+-- The current definition for the NFA's transition function
+-- returns a List, which is an infinite recursive type. The
+-- power set could more accurately be represented by the
+-- function (s -> Bool), which is equivalent to `2^s` in type
+-- algebra.
+--
+-- Although promising, I haven't yet been able to come up with
+-- a definition for `accepts` for this definition of δ.
+data TypedNFA s a = TypedNFA
+  { transitionT :: s -> Maybe a -> (s -> Bool),
+    startT :: s,
+    endingsT :: s -> Bool
+  }
+
 instance Automaton NFA where
   accepts :: Foldable t => NFA s a -> t a -> Bool
   accepts nfa string =
